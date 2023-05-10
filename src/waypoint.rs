420d1d2c004bd2dd0fa20325ba4abd740e19b4a3
Aryan Regmi <arre9260@protonmail.com>
2023-05-09 22:00:14 -0600

    Added waypoint struct

--- src/waypoint.rs
+++ src/waypoint.rs
@@ -15,9 +15,103 @@ pub(crate) enum WaypointType {
 #[derive(serde::Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Waypoint {
-    system_symbol: String,
+    // FIXME: Non-empty
     symbol: String,
     #[serde(rename = "type")]
     waypoint_type: WaypointType,
+    // FIXME: Non-empty
+    system_symbol: String,
+    x: i32,
+    y: i32,
+    orbitals: Vec<Symbol>,
+    faction: Option<Symbol>,
+    traits: Vec<Trait>,
+    chart: Option<Chart>,
 }
 
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Symbol {
+    symbol: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Trait {
+    symbol: TraitSymbols,
+    name: String,
+    description: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+enum TraitSymbols {
+    Uncharted,
+    Marketplace,
+    Shipyard,
+    Outpost,
+    ScatteredSettlements,
+    SprawlingCities,
+    MegaStructures,
+    Overcrowded,
+    HighTech,
+    Corrupt,
+    Bureaucratic,
+    TradingHub,
+    Industrial,
+    BlackMarket,
+    ResearchFacility,
+    MilitaryBase,
+    SurveillanceOutpost,
+    ExplorationOutpost,
+    MineralDeposits,
+    CommonMetalDeposits,
+    PreciousMetalDeposits,
+    RareMetalDeposits,
+    MethanePools,
+    IceCrystals,
+    ExplosiveGases,
+    StrongMagnetosphere,
+    VibrantAuroras,
+    SaltFlats,
+    Canyons,
+    PerpetualDaylight,
+    PerpetualOvercast,
+    DrySeabeds,
+    MagmaSeas,
+    Supervolcanoes,
+    AshClouds,
+    VastRuins,
+    MutatedFlora,
+    Terraformed,
+    ExtremeTemperatures,
+    ExtremePressure,
+    DiverseLife,
+    ScarceLife,
+    Fossils,
+    WeakGravity,
+    StrongGravity,
+    CrushingGravity,
+    ToxicAtmosphere,
+    CorrosiveAtmosphere,
+    BreathableAtmosphere,
+    Jovian,
+    Rocky,
+    Volcanic,
+    Frozen,
+    Swamp,
+    Barren,
+    Temperate,
+    Jungle,
+    Ocean,
+    Stripped,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Chart {
+    waypoint_symbol: String,
+    submitted_by: String,
+    // FIXME: Datetime
+    submitted_on: String,
+}
